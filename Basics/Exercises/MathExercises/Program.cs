2ed7cff6997ad5e282281ec032418c45175a51dd
pac09 <pco07@hotmail.com>
2020-06-25 22:08:57 -0600

    Cambios en programa consola para sumar dos matrices

--- Basics/Exercises/MathExercises/Program.cs
+++ Basics/Exercises/MathExercises/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MathExercises.Logic;
+
 namespace MathExercises
 {
     public class Program
@@ -46,19 +47,107 @@ namespace MathExercises
 
             //Console.ReadKey();
 
-            
-            PrimeNumbers prime = new PrimeNumbers();
-            List<int> numbers = prime.GetPrimeNumbers(1, 100);
+            //Codigo para imprimir numeros primos
+            //PrimeNumbers prime = new PrimeNumbers();
+            //List<int> numbers = prime.GetPrimeNumbers(1, 100);
 
-            foreach(int number in numbers)
+            //foreach(int number in numbers)
+            //{
+            //    Console.WriteLine("Los números primos son: " + number.ToString());
+            //}
+
+            //Console.ReadKey();
+
+            //Matriz de Informacion de Cliente
+            //List<ClientInfoMatrix> cif = new List<ClientInfoMatrix>();
+            //Console.WriteLine("¿Cuántos clientes deseas consultar?");
+            //string noClients = Console.ReadLine();
+
+            //int clients;
+            //Int32.TryParse(noClients, out clients);
+
+            //for (int i = 0; i < clients; i++) 
+            //{
+            //    //Esto instancia un nuevo objeto de la clase
+            //    ClientInfoMatrix newClient = new ClientInfoMatrix();
+
+            //    Console.WriteLine("Cual es su primer nombre?");
+            //    newClient.FirstName = Console.ReadLine();
+
+            //    Console.WriteLine("Cual es su primer apellido?");
+            //    newClient.LastName = Console.ReadLine();
+
+            //    Console.WriteLine("Cual es su edad?");
+            //    newClient.Age = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Cual es su fecha de nacimiento?");
+            //    newClient.Birthday = Convert.ToDateTime(Console.ReadLine());
+
+            //    //Agregar clientes
+            //    cif.Add(newClient);
+            //}
+
+            //Console.ReadKey();
+
+            //Ejecutar suma de Matrices
+            Console.WriteLine("Cuantas filas tiene la matriz A ?");
+            int rowNumbersA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cuantas columnas tiene la matriz A ?");
+            int columnNumbersA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cuantas filas tiene la matriz B ?");
+            int rowNumbersB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Cuantas columnas tiene la matriz B ?");
+            int columnNumbersB = Convert.ToInt32(Console.ReadLine());
+            
+            if (rowNumbersA == rowNumbersB && columnNumbersA == columnNumbersB)
             {
-                Console.WriteLine("Los números primos son: " + number.ToString());
+                int[,] matrixA = AsignData(rowNumbersA, columnNumbersA);
+                int[,] matrixB = AsignData(rowNumbersB, columnNumbersB);
+
+                int[,] matrixC = Sum(matrixA, matrixB); 
+            }
+            else
+            {
+                Console.WriteLine("Lo sentimos la matriz que esta intentando sumar no tienen la mismas dimensiones");
             }
 
             Console.ReadKey();
 
+            #region Local_Methods
+            int[,] AsignData(int rows, int columns)
+            {
+                int[,] matrix = new int[rows, columns];
 
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        Console.WriteLine("Cual es el valor para el elemento " + (i + 1).ToString() + (j + 1).ToString());
+                        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
 
+                return matrix;
+            }
+
+            int[,] Sum(int[,] matrixOne, int[,] matrixTwo)
+            {
+                int[,] matrix = new int[rowNumbersA, columnNumbersA];
+
+                for (int i = 0; i < rowNumbersA; i++) 
+                {
+                    for (int j = 0; j < columnNumbersA; j++)
+                    {
+                        matrix[i,j] = matrixOne[i,j] + matrixTwo[i,j];
+                    }
+                }
+
+                return matrix;
+            }
+            #endregion
         }
     }
 }
